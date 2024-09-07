513022c64dfb8d3a31ee2036ab368028e556004f
nvd11 <nvd11@163.com>
2024-09-07 22:48:47 +0800

    TEST

--- shell.sh
+++ shell.sh
@@ -1,2 +1,9 @@
 git pull && docker build --build-arg http_proxy=http://10.0.1.223:7890 --build-arg https_proxy=http://10.0.1.223:7890 -t gateman/sonarqube:1.0.0 .
 docker run -p 9000:9000 gateman/sonarqube:1.0.0
+
+
+sqa_acba1803e527317f966735b2ae664fab8b77753f
+
+sonar-scanner -Dsonar.host.url=http://10.0.1.223:9000 -Dsonar.projectKey=my:python-common  -Dsonar.sources=/home/gateman/projects/python/python_common -Dsonar.login=sqa_acba1803e527317f966735b2ae664fab8b77753f
+
+sonar-scanner -Dsonar.host.url=http://10.0.1.223:9000 -Dsonar.projectKey=my:python-common -Dsonar.sources=/home/gateman/projects/python/python_common -Dsonar.login=sqa_acba1803e527317f966735b2ae664fab8b77753f -Dsonar.exclusions=**/venv/**
